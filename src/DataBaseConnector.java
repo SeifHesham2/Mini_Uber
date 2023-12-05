4a5a9608a34ec835db9be3b021fc24209937eba9
seifhesham2 <seifhesham2030@gmail.com>
2023-12-05 19:07:45 +0200

    Database connection + new main

---
+++ src/DataBaseConnector.java
@@ -0,0 +1,16 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+public class DataBaseConnector
+{
+    public void  connectToDatabase(){
+   try (Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/MiniUber", "root", "123"))
+   {
+    System.out.println("Connected to the database");
+   }
+   catch (SQLException e)
+   {
+    System.out.println("Unable To connect To Database");
+   }
+                                      }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,10 @@
+import javax.xml.crypto.Data;
+
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("welcome to our project");
-
+        DataBaseConnector Database = new DataBaseConnector();
+        Database.connectToDatabase();
     }
 }
